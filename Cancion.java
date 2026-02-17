6d17dc503f53857e7d1c6609f08df6efdd8768c2
JuanesCordero <juanes141105@gmail.com>
2026-02-17 02:37:19 +0000

    Ajuste clase Cancion y ajuste interface IPlaylist

--- Cancion.java
+++
@@ -1,4 +0,0 @@
-public class Cancion{
-    string titulo;
-    int duracion;
-}
---
+++ punto 1/Cancion.java
@@ -0,0 +1,22 @@
+public class Cancion{
+    private String titulo;
+    private int duracion;
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public void setDuracion(int duracion){
+        this.duracion = duracion;
+    }
+
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public int getDuracion(){
+        return duracion;
+    }
+
+}
