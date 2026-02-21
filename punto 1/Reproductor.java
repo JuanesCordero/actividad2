8a33f67511ba60d4950507b200945ecd6fd307e4
JuanesCordero <juanes141105@gmail.com>
2026-02-21 01:04:41 +0000

    nueva clase principal para la actividad cancion

---
+++ punto 1/Reproductor.java
@@ -0,0 +1,7 @@
+public class Reproductor {
+    public static void main(String[]args){
+        Cancion pista = new Cancion();
+        pista.setDuracion(5);
+        pista.setTitulo("hinmo nacional");
+    }
+}
