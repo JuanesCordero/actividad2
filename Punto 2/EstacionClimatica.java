319d1968d529a3eceae6a7e777c9378761e64378
JuanesCordero <juanes141105@gmail.com>
2026-02-24 22:42:43 +0000

    cracion carpeta punto 2

---
+++ Punto 2/EstacionClimatica.java
@@ -0,0 +1,22 @@
+public class EstacionClimatica {
+    public static void main(String[] args) {
+
+        double[] temperaturas = new double[24];
+
+        for (int i = 0; i < temperaturas.length; i++) {
+            temperaturas[i] = Math.random() * 40;
+        }
+
+        System.out.println("Temperatura al mediodía (hora 12): " + temperaturas[12] + " °C");
+
+        try {
+            temperaturas[24] = 25.0; 
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("La memoria es fija en (0 a 23).");
+        }
+    }
+}
+
+
+    
+
