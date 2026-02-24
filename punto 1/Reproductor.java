1f13d813d16eb0796d16586dd08249d6ccbf04f2
JuanesCordero <juanes141105@gmail.com>
2026-02-24 15:34:51 +0000

    creacion impresion punto uno

--- punto 1/Reproductor.java
+++ punto 1/Reproductor.java
@@ -3,5 +3,8 @@ public class Reproductor {
         Cancion pista = new Cancion();
         pista.setDuracion(5);
         pista.setTitulo("hinmo nacional");
+        
+        System.out.println("Título: " + pista.getTitulo());
+        System.out.println("Duración: " + pista.getDuracion() + " minutos");
     }
 }
