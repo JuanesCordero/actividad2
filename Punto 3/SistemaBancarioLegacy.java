86224513d1c3f58d28d47ca8283b3f25a63bd3df
JuanesCordero <juanes141105@gmail.com>
2026-02-25 02:21:23 +0000

    Creacion comparacion de rendimiento

--- Punto 3/SistemaBancarioLegacy.java
+++ Punto 3/SistemaBancarioLegacy.java
@@ -8,15 +8,46 @@ public class SistemaBancarioLegacy {
         Vector<String> logTransacciones = new Vector<>();
 
         logTransacciones.add("Transferencia de $500 realizada.");
-        logTransacciones.add("Depósito de $1200 realizado.");
-        logTransacciones.add("Retiro de $300 realizado.");
-        logTransacciones.add("Pago de servicios $150 realizado.");
-        logTransacciones.add("Consulta de saldo realizada.");
+        logTransacciones.add("Depósito de $1200.");
+        logTransacciones.add("Retiro de $300.");
+        logTransacciones.add("Pago de servicios $150 .");
+        logTransacciones.add("Consulta de saldo.");
 
         System.out.println("REGISTRO");
         for (String log : logTransacciones) {
             System.out.println(log);
         }
+        
+         public static void compararRendimiento() {
+
+        int cantidad = 1_000_000;
+
+        // 🔹 Medición con Vector
+        Vector<Integer> vector = new Vector<>();
+        long inicioVector = System.currentTimeMillis();
+
+        for (int i = 0; i < cantidad; i++) {
+            vector.add(i);
+        }
+
+        long finVector = System.currentTimeMillis();
+        long tiempoVector = finVector - inicioVector;
+
+        // 🔹 Medición con ArrayList
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        long inicioArray = System.currentTimeMillis();
+
+        for (int i = 0; i < cantidad; i++) {
+            arrayList.add(i);
+        }
+
+        long finArray = System.currentTimeMillis();
+        long tiempoArray = finArray - inicioArray;
+
+        System.out.println("\n=== COMPARACIÓN DE RENDIMIENTO ===");
+        System.out.println("Tiempo Vector: " + tiempoVector + " ms");
+        System.out.println("Tiempo ArrayList: " + tiempoArray + " ms");
+    }
 
     }
 }
